1c1d08966d7d3472820f7963a78c3873c9ab3e79
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-09 11:40:19 +0930

    add timeTwo

--- 01_WhenAny/01_WhenAny/MainViewModel.cs
+++ 01_WhenAny/01_WhenAny/MainViewModel.cs
@@ -11,10 +11,15 @@ namespace _01_ReactiveUITest
     public class MainViewModel : ReactiveObject
     {
         private DateTimeOffset _time;
+        private DateTimeOffset _timeTwo;
 
         public MainViewModel()
         {
-            this.WhenAnyValue(x => x.Time)
+            this.WhenAnyValue(
+                    x => x.Time,
+                    x => x.TimeTwo,
+                    (time, timeTwo) => $"Time 1: {time}\nTime 2: {timeTwo}")
+                .ObserveOn(RxApp.MainThreadScheduler)
                 .Subscribe(time => System.Console.WriteLine(time));
 
             Observable
@@ -26,12 +31,28 @@ namespace _01_ReactiveUITest
                         Time = time;
                     }
                 );
+
+            Observable
+                .Interval(TimeSpan.FromSeconds(2))
+                .Select(_ => DateTimeOffset.Now)
+                .Subscribe(
+                    time =>
+                    {
+                        TimeTwo = time;
+                    }
+                );
         }
 
         public DateTimeOffset Time
         {
             get => _time;
             set => this.RaiseAndSetIfChanged(ref _time, value);
+        }
+
+        public DateTimeOffset TimeTwo
+        {
+            get => _timeTwo;
+            set => this.RaiseAndSetIfChanged(ref _timeTwo, value);
         }
     }
 } 
